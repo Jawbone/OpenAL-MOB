fa1f85d236b9ebaf2de20d53848dd676f542878f
infinitap <git@infinitapgames.com>
2013-09-13 23:51:53 +0800

    Set license file back to LGPL

--- include/AL/alconflict.h
+++ include/AL/alconflict.h
@@ -1,3 +1,25 @@
+/**
+ * OpenAL-MOB cross platform audio library
+ * Copyright (C) 2013 by Jawbone.
+ * This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA  02111-1307, USA.
+ * Or go to http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This file is unique to OpenAL-MOB and is not part of the OpenAL-Soft package.
+ */
+
 // If you set this to 1, then it will rename prefix all al*() functions with ALMOB_. This allows you
 // to link two separate OpenAL implementations into the same program.
 #define ALMOB_UNIQUE_NAMES     (0)
